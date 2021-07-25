17057f2a815db5ddd18ac1617e518dc0d53cf6ba
chunza2542 <hello@thechun.dev>
2021-07-25 12:23:48 +0700

    Setup tslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
-import React from 'react';
+import React from "react";
 
 function App() {
-  return (
-    <div>
-      Hello World
-    </div>
-  );
+  return <div>Hello World</div>;
 }
 
 export default App;
