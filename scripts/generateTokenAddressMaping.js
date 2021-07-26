2dd8e0cf48dc02f1a0180959c3106809bf0acb1a
chunza2542 <hello@thechun.dev>
2021-07-26 11:56:36 +0700

    Generate token contract address mapping file

---
+++ scripts/generateTokenAddressMaping.js
@@ -0,0 +1,47 @@
+const https = require("https");
+const fs = require("fs");
+
+const options = {
+  hostname: "api.coingecko.com",
+  port: 443,
+  path: "/api/v3/coins/list?include_platform=true",
+  method: "GET",
+};
+
+const process = (data) => {
+  return data
+    .filter(
+      (d) => d.platforms.ethereum !== undefined && d.platforms.ethereum !== ""
+    )
+    .reduce((result, curr) => {
+      const { id, name } = curr;
+      result[curr.platforms.ethereum] = {
+        id,
+        name,
+      };
+      return result;
+    }, {});
+};
+
+const req = https.request(options, (res) => {
+  const chunks = [];
+
+  res.on("data", (d) => {
+    chunks.push(d);
+  });
+
+  res.on("end", () => {
+    const data = JSON.parse(Buffer.concat(chunks).toString());
+    const processedData = process(data);
+    const filepath = "./src/repos/tokenAddressMapping.json";
+
+    fs.writeFileSync(filepath, JSON.stringify(processedData, 2, 2));
+    console.log(`[DONE] Generated token address mapping file to ${filepath}`);
+  });
+});
+
+req.on("error", (error) => {
+  console.error(`[Error] ${error.message}`);
+});
+
+req.end();
