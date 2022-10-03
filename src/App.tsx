a0ce38b18c0579c586e9635955e418c6e4d8f915
chunza2542 <hello@thechun.dev>
2022-10-03 20:04:00 +0700

    Remove responsive warning modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import styled from "styled-components";
-import { useWindowWidth } from "@react-hook/window-size";
 import { Br } from "./common/atomic";
 import CorrelationChart from "./containers/CorrelationChart";
 import Credit from "./containers/Credit";
@@ -10,11 +9,6 @@ import Navbar from "./containers/Navbar";
 import LiquidityPositionChart from "./containers/LiquidityPositionChart";
 import SelectPairModal from "./containers/select-pair/SelectPairModal";
 import Setting from "./containers/setting/Setting";
-import { PrimaryBlockButton } from "./common/buttons";
-import { useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGithub, faTwitter } from "@fortawesome/free-brands-svg-icons";
-import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 // import { useModalContext } from "./context/modal/modalContext";
 // import AnnoucementModal from "./containers/AnnoucementModal";
 // import { ModalActionType } from "./context/modal/modalReducer";
@@ -30,49 +24,8 @@ const ContentContainer = styled.div`
   grid-gap: 25px;
   margin-top: 25px;
 `;
-const MobileNotSupportScreen = styled.div`
-  width: 100vw;
-  height: 100vh;
-  position: relative;
-  top: 0;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-  padding: 15px;
-  text-align: center;
-
-  color: white;
-
-  & > h1 {
-    margin: 0;
-    font-size: 1.2rem;
-  }
-  & > p {
-    color: #ccc;
-    font-size: 1rem;
-  }
-`;
-const EnterSite = styled.div`
-  margin-top: 15px;
-  width: calc(100vw - 30px);
-`;
-const Unicorn = styled.div`
-  font-size: 5rem;
-`;
-const IconBar = styled.div`
-  font-size: 1.2rem;
-  margin-top: 30px;
-
-  & > a {
-    color: #777;
-    margin: 0 10px;
-  }
-`;
 
 function App() {
-  const screenWidth = useWindowWidth();
-  const [isEnter, setIsEnter] = useState(false);
   // const { state, dispatch } = useModalContext();
 
   // useEffect(() => {
@@ -86,66 +39,27 @@ function App() {
 
   return (
     <>
-      {!isEnter && screenWidth <= 960 && (
-        <MobileNotSupportScreen>
-          <Unicorn>🦄</Unicorn>
-          <h1>Welcome to UniswapCalculator</h1>
-          <p>
-            Hi 🖐 Welcome to UniswapCalculator! I just want to tell you that
-            this site doesn't support mobile devices yet. for a better
-            experience, please visit on your desktop
-          </p>
-          <EnterSite>
-            <PrimaryBlockButton onClick={() => setIsEnter(true)}>
-              I understand, let's enter the site!
-            </PrimaryBlockButton>
-          </EnterSite>
-          <IconBar>
-            <a
-              href="https://twitter.com/chunza2542"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <FontAwesomeIcon icon={faTwitter} />
-            </a>
-            <a
-              href="https://github.com/chunza2542/uniswapv3-calculator"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <FontAwesomeIcon icon={faGithub} />
-            </a>
-            <a href="mailto:hello@thechun.dev">
-              <FontAwesomeIcon icon={faEnvelope} />
-            </a>
-          </IconBar>
-        </MobileNotSupportScreen>
-      )}
-      {(isEnter || screenWidth > 960) && (
-        <>
-          <SelectPairModal />
-          {/* <AnnoucementModal /> */}
-          {/* <DonateModal /> */}
-          <Navbar />
-          <BodyContainer>
-            <Header />
-            <ContentContainer>
-              <div>
-                <EstimatedFees />
-                <Br />
-                <Setting />
-              </div>
-              <div>
-                <LiquidityPositionChart />
-                <Br />
-                <CorrelationChart />
-              </div>
-            </ContentContainer>
+      <SelectPairModal />
+      {/* <AnnoucementModal /> */}
+      {/* <DonateModal /> */}
+      <Navbar />
+      <BodyContainer>
+        <Header />
+        <ContentContainer>
+          <div>
+            <EstimatedFees />
+            <Br />
+            <Setting />
+          </div>
+          <div>
+            <LiquidityPositionChart />
+            <Br />
+            <CorrelationChart />
+          </div>
+        </ContentContainer>
 
-            <Credit />
-          </BodyContainer>
-        </>
-      )}
+        <Credit />
+      </BodyContainer>
     </>
   );
 }
