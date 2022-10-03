172cd36876665032683f21f604c4f1bdfac93b9e
chunza2542 <hello@thechun.dev>
2022-10-03 16:02:07 +0700

    Update tokenAddressMapping generated structure

--- scripts/generateTokenAddressMaping.js
+++ scripts/generateTokenAddressMaping.js
@@ -8,20 +8,52 @@ const options = {
   method: "GET",
 };
 
-// TODO: refactor, there some case that can cause id override e.g. WETH
 const SKIP_PLATFORMS = [
-  "ethereumpow",
-  "cosmos",
   "tron",
+  "sora",
+  "factom",
+  "elrond",
+  "osmosis",
+  "cosmos",
+  "nuls",
+  "everscale",
+  "stacks",
+  "vite",
+  "bitcoin-cash",
+  "nem",
+  "tezos",
+  "hedera-hashgraph",
+  "mixin-network",
+  "hydra",
+  "xrp",
+  "eos",
+  "binancecoin",
+  "near-protocol",
+  "terra",
+  "secret",
+  "neo",
+  "algorand",
+  "stellar",
+  "cardano",
+  "karura",
+  "fusion-network",
+  "zilliqa",
+  "icon",
+  "solana",
+  "xdc-network",
+  "waves",
 ];
 
 const process = (data) => {
   return data.reduce((result, curr) => {
     const { id, name } = curr;
-    Object.keys(curr.platforms).map((key) => {
-      if (!SKIP_PLATFORMS.includes(key)) {
-        result[curr.platforms[key]] = { id, name };
-      }
+    Object.keys(curr.platforms).map((platform) => {
+      if (SKIP_PLATFORMS.includes(platform)) return;
+      const address = curr.platforms[platform].trim();
+      if (!address) return;
+
+      if (!result[platform]) result[platform] = {};
+      result[platform][address] = { id, name };
     });
     return result;
   }, {});
@@ -37,9 +69,11 @@ const req = https.request(options, (res) => {
   res.on("end", () => {
     const data = JSON.parse(Buffer.concat(chunks).toString());
     const processedData = process(data);
+
     const filepath = "./src/repos/tokenAddressMapping.json";
 
     fs.writeFileSync(filepath, JSON.stringify(processedData, 2, 2));
+    console.log(`[Log] Chains: ${Object.keys(processedData).join(", ")}`);
     console.log(`[DONE] Generated token address mapping file to ${filepath}`);
   });
 });
