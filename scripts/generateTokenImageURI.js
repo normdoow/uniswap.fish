592186e9b4dcc487284aad3d206619fde6ecf88f
chunza2542 <hello@thechun.dev>
2022-10-05 15:21:32 +0700

    Add new logoURLs to list

--- scripts/generateTokenImageURI.js
+++ scripts/generateTokenImageURI.js
@@ -3,16 +3,17 @@ const axios = require("axios");
 
 const URLS = [
   "https://tokens.uniswap.org",
-  "https://bridge.arbitrum.io/token-list-42161.json",
-  "https://static.optimism.io/optimism.tokenlist.json",
-  "https://api.coinmarketcap.com/data-api/v3/uniswap/all.json",
-  "https://raw.githubusercontent.com/compound-finance/token-list/master/compound.tokenlist.json",
-  "https://raw.githubusercontent.com/SetProtocol/uniswap-tokenlist/main/set.tokenlist.json",
-  "https://app.tryroll.com/tokens.json",
-  "https://tokens.coingecko.com/uniswap/all.json",
-  "https://static.optimism.io/optimism.tokenlist.json",
-  "https://www.gemini.com/uniswap/manifest.json",
+  "https://extendedtokens.uniswap.org",
+  "https://unsupportedtokens.uniswap.org",
   "https://raw.githubusercontent.com/The-Blockchain-Association/sec-notice-list/master/ba-sec-list.json",
+  "https://api.coinmarketcap.com/data-api/v3/uniswap/all.json",
+  "https://tokens.coingecko.com/uniswap/all.json",
+  "https://raw.githubusercontent.com/compound-finance/token-list/master/compound.tokenlist.json",
+  "https://www.gemini.com/uniswap/manifest.json",
+  "https://app.tryroll.com/tokens.json",
+  "https://raw.githubusercontent.com/SetProtocol/uniswap-tokenlist/main/set.tokenlist.json",
+  "https://static.optimism.io/optimism.tokenlist.json",
+  "https://bridge.arbitrum.io/token-list-42161.json",
 ];
 
 Promise.all(URLS.map((url) => axios.get(url)))
@@ -23,7 +24,11 @@ Promise.all(URLS.map((url) => axios.get(url)))
     return tokens
       .filter((t) => t !== undefined && t.logoURI)
       .reduce((result, curr) => {
-        result[curr.address.toLowerCase()] = curr.logoURI;
+        let logoURI = curr.logoURI;
+        if (logoURI.indexOf("ipfs://") !== -1) {
+          logoURI = logoURI.replace(`ipfs://`, `https://ipfs.io/ipfs/`);
+        }
+        result[curr.address.toLowerCase()] = logoURI;
         return result;
       }, {});
   })
