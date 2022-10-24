f162dac04b5182dba08294af01aac5fe076c4547
chunza2542 <hello@thechun.dev>
2022-10-24 15:26:59 +0700

    Create LOG_WEB_VITALS flag

--- src/index.tsx
+++ src/index.tsx
@@ -19,9 +19,10 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+const LOG_WEB_VITALS = false;
 const reportHandler = (metric: Metric) => {
-  if (process.env.NODE_ENV === "development") {
-    console.log(metric);
+  if (LOG_WEB_VITALS && process.env.NODE_ENV === "development") {
+    console.log("web-vitals", metric);
   }
 };
 
