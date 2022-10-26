84027139b119b794d8dab62008acae13dc9640fe
chunza2542 <hello@thechun.dev>
2022-10-26 13:42:19 +0700

    Update Twitter icon

--- src/containers/Navbar.tsx
+++ src/containers/Navbar.tsx
@@ -1,9 +1,7 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGithub, faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { faTwitter } from "@fortawesome/free-brands-svg-icons";
 import styled from "styled-components";
-import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
-import { DangerButton } from "../common/buttons";
 import { ScreenWidth } from "../utils/styled";
 // import { useModalContext } from "../context/modal/modalContext";
 // import { ModalActionType } from "../context/modal/modalReducer";
@@ -53,77 +51,33 @@ const Logo = styled.h1`
 const Menubar = styled.div`
   display: flex;
   align-items: center;
-
-  & a {
-    color: rgba(255, 255, 255, 0.6);
-    font-size: 1.2rem;
-    margin-right: 15px;
-
-    &:hover {
-      color: rgba(255, 255, 255, 0.8);
-    }
-  }
 `;
-const Gitcoin = styled.div`
-  position: relative;
-  margin-right: -15px;
 
-  & p {
-    font-size: 0.7em;
-    color: white;
-    margin: 0;
-    position: absolute;
-    width: 250px;
-    right: 0;
-    margin-top: 8px;
+const Twitter = styled.a`
+  color: rgba(255, 255, 255, 0.6);
+  font-size: 1.2rem;
+  margin-right: 15px;
+
+  &:hover {
+    color: #1f9cea;
+    transform: scale(1.25) rotate(18deg);
   }
 `;
 
 const Navbar = () => {
-  // const { dispatch } = useModalContext();
-
   return (
     <NavbarContainer>
       <Logo>
         <span>🦄</span> <span>UniswapCalculator</span>
       </Logo>
       <Menubar>
-        <a
+        <Twitter
           href="https://twitter.com/uniswapdotfish"
           target="_blank"
           rel="noreferrer"
         >
           <FontAwesomeIcon icon={faTwitter} />
-        </a>
-        <a
-          href="https://github.com/chunza2542/uniswap.fish"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <FontAwesomeIcon icon={faGithub} />
-        </a>
-        <a href="mailto:hello@uniswap.fish">
-          <FontAwesomeIcon icon={faEnvelope} />
-        </a>
-        <a
-          href="https://gitcoin.co/grants/4203/uniswap-calculator-v3"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <Gitcoin>
-            <DangerButton
-            // onClick={() => {
-            //   dispatch({
-            //     type: ModalActionType.SET_DONATE_MODAL_STATE,
-            //     payload: true,
-            //   });
-            // }}
-            >
-              <span>Donate</span>
-            </DangerButton>
-            {/* <p>🔥 Support our project on Gitcoin GR15!</p> */}
-          </Gitcoin>
-        </a>
+        </Twitter>
       </Menubar>
     </NavbarContainer>
   );
