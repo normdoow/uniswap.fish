1ca8d83a8d3f9c4cd2303562c6af113dcf858efd
chunza2542 <hello@thechun.dev>
2022-11-28 00:14:59 +0700

    Setup Plausible Plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,12 @@
 module.exports = {
   plugins: [
     {
+      resolve: `gatsby-plugin-plausible`,
+      options: {
+        domain: `uniswap.fish`,
+      },
+    },
+    {
       resolve: `gatsby-plugin-global-styles`,
       options: {
         pathToConfigModule: `src/GlobalStyle`,
