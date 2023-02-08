81e11bf55eb8e222fea05ccee57ea5d2a2aab46c
chunza2542 <hello@thechun.dev>
2023-02-08 11:50:28 +0700

    Add tooltip for Create Position modal

--- src/containers/CreatePositionModal.tsx
+++ src/containers/CreatePositionModal.tsx
@@ -3,8 +3,9 @@ import Modal from "react-modal";
 import { useModalContext } from "../context/modal/modalContext";
 import styled from "styled-components";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCopy, faTimes } from "@fortawesome/free-solid-svg-icons";
+import { faQuestionCircle, faTimes } from "@fortawesome/free-solid-svg-icons";
 import { ModalActionType } from "../context/modal/modalReducer";
+import ReactTooltip from "react-tooltip";
 
 const ModalStyle = {
   overlay: {
@@ -25,14 +26,14 @@ const ModalStyle = {
   },
 };
 const Container = styled.div`
-  width: 470px;
+  width: 370px;
   padding: 15px;
 `;
 const Header = styled.h1`
   color: white;
   margin: 0;
   font-weight: bold;
-  font-size: 1.2rem;
+  font-size: 1.1rem;
   color: #ddd;
   font-weight: 500;
   display: flex;
@@ -45,7 +46,7 @@ const Header = styled.h1`
   color: white;
 
   & span:nth-child(1) {
-    font-size: 1.2rem;
+    font-size: 1.1rem;
   }
   & span:nth-child(2) {
     cursor: pointer;
@@ -53,44 +54,13 @@ const Header = styled.h1`
   }
 `;
 
-const Content = styled.div`
-  color: #ccc;
-
-  & a {
-    color: rgb(30, 161, 241);
-    text-decoration: none;
-    font-weight: 600;
-  }
-`;
-const DonationBox = styled.div`
-  background: rgba(255, 255, 255, 0.1);
-  border-radius: 12px;
-  padding: 8px;
-  padding-top: 3px;
-
-  & > span {
-    font-weight: bold;
-    font-size: 0.8rem;
-  }
-  & > div {
-    padding: 3px 7px;
-    margin-top: 5px;
-    background: rgba(255, 255, 255, 0.1);
-    border-radius: 5rem;
-    display: flex;
-    justify-content: space-between;
-    cursor: pointer;
-
-    &:hover {
-      background: rgba(255, 255, 255, 0.2);
-    }
-
-    & > div {
-      cursor: pointer;
-      margin-right: 5px;
-    }
-  }
-`;
+const DepositAmountSection = () => {
+  return (
+    <>
+      <span>Deposit Amount</span>
+    </>
+  );
+};
 
 const CreatePositionModal = () => {
   const { state, dispatch } = useModalContext();
@@ -100,12 +70,25 @@ const CreatePositionModal = () => {
       <Modal
         style={ModalStyle}
         isOpen={state.isCreatePositionModalOpen}
-        contentLabel="DONATE"
+        contentLabel="CREATE POSITION"
         ariaHideApp={false}
       >
         <>
+          <ReactTooltip id="create-position" />
           <Header>
-            <span>💰 DONATE</span>
+            <span>
+              Create Position
+              <FontAwesomeIcon
+                style={{ opacity: 0.3, marginLeft: 5 }}
+                data-for="create-position"
+                data-place="bottom"
+                data-html={true}
+                data-tip={`This feature will instruct you how to create a position and help you calculate the deposit tokens amount for current price range setting.
+                <br>
+                Noted that there is no smart contract risk evolve using this feature since you will be the one who creates a new position yourself on the official Uniswap interface. This feature only gives you "Instruction."`}
+                icon={faQuestionCircle}
+              />
+            </span>
             <span
               onClick={() => {
                 dispatch({
@@ -118,49 +101,7 @@ const CreatePositionModal = () => {
             </span>
           </Header>
           <Container>
-            <Content>
-              <p>
-                Hi! 🖐 my name is{" "}
-                <a href="https://twitter.com/chunrapeepat" target="_blank">
-                  Chun
-                </a>
-                , 22 y/o software developer, indie maker. It's been 2 weeks now
-                since I getting to learn Blockchain, Ethereum (and of course
-                Uniswap 🦄!) asides from my full-time job and this was the first
-                project that I've built to learn more about Uniswap after using
-                it for a while.
-              </p>
-              <p>
-                So!, If you love this project (and want to see a lot more
-                projects that I've planned to build in the near future), feel
-                free to donate many{" "}
-                <span style={{ fontWeight: 600, color: "rgb(134, 142, 173)" }}>
-                  ETH
-                </span>{" "}
-                or{" "}
-                <span style={{ fontWeight: 600, color: "rgb(247, 8, 129)" }}>
-                  UNI
-                </span>{" "}
-                you want. your donation will go to my learning and investment
-                budgets which will help me a lot to make this become career 😎
-              </p>
-              <DonationBox>
-                <span>ETHEREUM ADDRESS</span>
-                <div
-                  onClick={() => {
-                    navigator.clipboard.writeText(
-                      "0x79A375feFbF90878502eADBA4A89697896B60c4d"
-                    );
-                  }}
-                >
-                  <span>0x79A375feFbF90878502eADBA4A89697896B60c4d</span>
-                  <div>
-                    <FontAwesomeIcon icon={faCopy} />
-                  </div>
-                </div>
-              </DonationBox>
-              <p>Thanks and happy Uniswapping 🦄!</p>
-            </Content>
+            <DepositAmountSection />
           </Container>
         </>
       </Modal>
